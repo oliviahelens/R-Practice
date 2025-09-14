0c4ee4af93b00f8346854a86a9447ba089907f21
oliviahelens <oliviascharfman@gmail.com>
2025-09-14 02:57:35 +0000

    Removed commented-out code

--- Life Expectancy by Country/Life-Expectancy.r
+++ Life Expectancy by Country/Life-Expectancy.r
@@ -11,11 +11,9 @@ print(head(data))
 # life expectancy
 
 life_expectancy <- data %>% pull(life_expectancy)
-# print(life_expectancy)
 
 # life expectancy quartiles
 life_expectancy_quartiles <- quantile(life_expectancy, c(.25, .5, .75))
-# print(life_expectancy_quartiles)
 
 # plot histogram of life expectancy
 hist(life_expectancy)
@@ -23,11 +21,8 @@ hist(life_expectancy)
 # gdp
 gdp <- data %>% pull(GDP)
 
-# print(gdp)
-
 # median gdp
 median_gdp <- median(gdp)
-# print(median_gdp)
 
 # low gdp
 low_gdp <- data %>%  filter(GDP <= median_gdp)
@@ -35,11 +30,9 @@ low_gdp <- data %>%  filter(GDP <= median_gdp)
 # high gdp
 high_gdp <- data %>% filter(GDP > median_gdp)
 
-
 # low gdp quartiles
 
 lev_low_gdp <- data %>% filter(GDP <= median_gdp) %>% pull(life_expectancy)
-# print(lev_low_gdp)
 
 # high gdp quartiles
 
